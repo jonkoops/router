0508c6a7615c3056a38c972c4212972d0c03d771
Jon Koops <jonkoops@gmail.com>
2024-10-27 16:23:33 +0100

    refactor!: replace `is-promise` dependency with `instanceof` check
    BREAKING: drops support for non-native promises
    
    Closes #136
    
    Signed-off-by: Jon Koops <jonkoops@gmail.com>

--- index.js
+++ index.js
@@ -12,7 +12,6 @@
  * @private
  */
 
-const isPromise = require('is-promise')
 const Layer = require('./lib/layer')
 const methods = require('methods')
 const mixin = require('utils-merge')
@@ -639,7 +638,7 @@ function processParams (params, layer, called, req, res, done) {
 
     try {
       const ret = fn(req, res, paramCallback, paramVal, key)
-      if (isPromise(ret)) {
+      if (ret instanceof Promise) {
         ret.then(null, function (error) {
           paramCallback(error || new Error('Rejected promise'))
         })
